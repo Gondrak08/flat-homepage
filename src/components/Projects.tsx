8243395ca3bb6c66fa3ca71853dd9d5fd63b7d91
=vitor <vitor.alecrim.p@gmail.com>
2024-10-09 20:03:51 -0300

    2.3.0

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,21 +1,34 @@
+import { useState } from 'react';
 import { useTranslations } from "next-intl";
 import Card from "./Card";
 import Works from '@/data/works';
+import ProjectsData from '@/data/projects';
 
 export default function Projects() {
+    const [tab, setTab] = useState<string>('projects');
     const t = useTranslations("Projects")
+
     return (
         <section id="projects"
-        className="animation flex flex-col gap-2 justify-center w-full h-full px-5 md:px-0 ">
-            <h3  className=" text-xl md:text-2xl font-bold 
-            underline underline-offset-4 text-black" >
-                {t("title")}
-            </h3>
+            className="animation flex flex-col  gap-5  w-full h-full px-5 md:px-0 ">
+            <div className='flex w-full items-center gap-5 border border-b-black '>
+                <h3 className={`text-xl md:text-2xl font-bold 
+              border border-black border-b-[0]  rounded-t-xl p-2 relative hover:cursor-pointer ${tab !=='projects' ? 'text-gray-700 hover:text-black': 'text-black' }`} onClick={() => { setTab('projects') }} >
+                    {t("title one")}
+                    {tab == 'projects' && <span className='absolute left-0 bottom-[-2px] w-full border border-[rgb(225 229 230)]' />}
+                </h3>
+
+                <h3 className={`text-xl md:text-2xl font-bold 
+              border border-black border-b-[0]  rounded-t-xl p-2 relative hover:cursor-pointer ${tab !=='collabs' ? 'text-gray-700 hover:text-black': 'text-black' }`} onClick={() => { setTab('collabs') }} >
+                    {t("title two")}
+                    {tab == 'collabs' && <span className='absolute left-0 bottom-[-2px] w-full border border-[rgb(225 229 230)]' />}
+                </h3>
+            </div>
             <div id="works-display" className="grid grid-cols-1 md:grid-cols-2
              gap-5 py-4 md:mx-[-3em] " >
-                {
-                    Works.map((work, index:number)=>(
-                         <Card 
+                {tab == 'projects' &&
+                    ProjectsData.map((work: any, index: number) => (
+                        <Card
                             key={index}
                             title={work.Title}
                             description={work.description}
@@ -25,8 +38,24 @@ export default function Projects() {
                             repo={work.code}
                         />
                     ))
+
                 }
-            </div>    
+                {tab == 'collabs' &&
+                    Works.map((work: any, index: number) => (
+                        <Card
+                            key={index}
+                            title={work.Title}
+                            description={work.description}
+                            image={work.img}
+                            stacks={work.stacks}
+                            link={work.demo}
+                            repo={work.code}
+                        />
+                    ))
+
+                }
+
+            </div>
         </section>
     )
 };
--- src/data/works.js
+++ src/data/works.js
@@ -1,163 +1,44 @@
-export const storeCards = [
-  {
-    id: 113,
-    Title: "eCommerce Store",
-    img: "fakestore.png",
-    description: "A demo eCommerce Store. Freed by Fake Store Api",
-    stacks: ["Next.js", "javascript", "Typescript", "TailwindCss"],
-    demo: "https://amazing-duckanoo-88250c.netlify.app/",
-    code: "",
-  },
-  {
-    id: 112,
-    Title: "Laywer web marketing page",
-    img: "singlepagelaywer.png",
-    description: "A demo marketing landing page for a laywer",
-    stacks: ["Next.js", "javascript", "Typescript", "TailwindCss"],
-    demo: "https://glittering-torte-dade76.netlify.app/",
-    code: "",
-  },
-  {
-    id: 111,
-    Title: "M & M",
-    img: "lawyerweb.png",
-    description: "A demo landing-page for a law firm or laywer.",
-    stacks: ["Next.js", "javascript", "Typescript", "TailwindCss"],
-    demo: "https://calm-sfogliatella-7dcffc.netlify.app/",
-    code: "https://github.com/Gondrak08/lawyer-demo",
-  },
-  {
-    id: 0,
-    Title: "Furniture Store",
-    img: "furniturestore.png",
-    description:
-      "A demo landing-page for a Furniture store based on a real website, homestory",
-    stacks: ["Reactjs", "React-Redux", "javascript", "Css"],
-    demo: "https://furniturestore-demo.netlify.app/",
-    code: "https://github.com/Gondrak08/furnitureStore",
-  },
+export const Works = [
   {
     id: 1,
-    Title: "Digital Menu",
-    img: "menuapp.png",
-    description: "A digital menu prototype for a restaurant.",
-    stacks: [
-      "Next.js",
-      "React-Redux",
-      "Typescript",
-      "javascript",
-      "TailwindCss",
-    ],
-    demo: "https://demodigitalmenu.netlify.app/",
-    code: "https://github.com/Gondrak08/menu-app",
+    Title: "Licita Connect",
+    img: "licitaconnect.png",
+    description:
+      "A An auction platform website for public bidding, designed for municipalities in Brazil",
+    stacks: [],
+    demo: "http://sistema.licitaconnect.com.br/",
+    code: "",
   },
   {
     id: 2,
-    Title: "Watch Shop",
-    img: "watchshop.png",
+    Title: "Acervo Furnas",
+    img: "acervofurnas.png",
     description:
-      "A fast single-page application made for a watch-shop. You can see the demo bellow.",
-    stacks: ["Nextjs", "javascript", "TailwindCss"],
-    demo: "https://watchesdemoshop.netlify.app/",
+      "A historical file collection website for the company Eletrobras. I developed this project during my part-time at Plano_B.",
+    stacks: [],
+    demo: "https://acervofurnas.com.br/",
     code: "",
   },
   {
-    id: 6,
-    Title: "JSON SCHEMA VALIDATOR",
-    img: "jsonSchema.gif",
-    description:
-      "This is an open-source application capable of generating a form from a JSON Schema object and exporting the data provided by the user as a JSON object.",
-    stacks: ["reactjs", "javascript", "scss", "open-source"],
-    demo: "https://endearing-zuccutto-3e880d.netlify.app",
-    code: "https://github.com/Gondrak08/JSONSchema-validator",
-  },
-  {
-    id: 5,
-    Title: "Crappo demo website",
-    img: "crappo.png",
-    description:
-      "This is a demo website for an e-coin company, adapted from a Figma design found on the Dribbble website and built using the latest technologies. Feel free to explore the site.",
-    stacks: ["reactjs", "javascript", "tailwindcss", "figma"],
-    demo: "https://crappodemo.netlify.app/#",
-    code: "https://github.com/Gondrak08/crappo-website",
-  },
-  {
     id: 3,
-    Title: "Dominos share link",
-    img: "dominos.png",
-    description: "Dominos share page for a domino's franchise",
-    stacks: ["html", "javascript", "css"],
-    demo: "https://dominosbrotas.netlify.app/",
+    Title: "Projeto Evandro Carneiro",
+    img: "evandrocarneiro.png",
+    description:
+      "A collection website for the works of the conceptual Brazilian artist Evandro Caneiro.",
+    stacks: [],
+    demo: "https://projetoevandrocarneiro.com.br/",
     code: "",
   },
   {
     id: 4,
-    Title: "Store managment System",
-    img: "smsystem.png",
+    Title: "Memória da Eletricidade",
+    img: "memoriaeletricidade.png",
     description:
-      "This is a customizable management system designed for small retail stores. It can be tailored to meet the specific needs of the user. Please feel free to try the demo version below using the login credentials: guest@admin.com (email) and guest (password).",
-    stacks: [
-      "reactjs",
-      "javascript",
-      "tailwindcss",
-      "nodeJs",
-      "mysql",
-      "heroku",
-      "Auth",
-    ],
-    demo: "https://smsystemdemo.netlify.app/",
+      "Website for Eletrobras' historical archive, the largest power plant company in South America",
+    stacks: [],
+    demo: "https://memoriadaeletricidade.com.br/",
     code: "",
-  },
-
-  {
-    id: 1,
-    Title: "Travell app",
-    img: "travelapp.png",
-    description:
-      "A travel webapp capable of calculating the points between the origin city, final destiny and intermediate city.",
-    stacks: ["Reactjs", "Typscrip", "javascript", "Tailwindcss"],
-    demo: "https://traveldemotask.netlify.app",
-    code: "",
-  },
-
-  {
-    id: 7,
-    Title: "Authentication Login",
-    img: "register_small.gif",
-    description:
-      "This is a web application and an API for user authentication, allowing for user registration and login. The frontend is built with TypeScript and utilizes context. The authentication API is built with Node.js and uses MongoDB as the database.",
-    stacks: ["reactjs", "javascript", "typescript", "nodejs", "mongoDB", "css"],
-    demo: "",
-    code: "https://github.com/Gondrak08/authentication-login",
-  },
-  {
-    id: 8,
-    Title: "Instagram Clone",
-    img: "instagram.jpg",
-    description:
-      "This is an instagram clone, with webapplication, mobile and backend. Frontend made with React.js. Mobile made with React-native. Backend made with NodeJs and mongoDb",
-    stacks: [
-      "reactjs",
-      "react-native",
-      "javascript",
-      "expo",
-      "nodejs",
-      "mongoDB",
-    ],
-    demo: "",
-    code: "https://github.com/Gondrak08/igclone",
-  },
-
-  {
-    id: 9,
-    Title: "Asteroid game",
-    img: "asteroid.png",
-    description:
-      "Asteroid game, like atari, made with vanilla javascript, html and css",
-    stacks: ["html", "css", "javascript", "git", "github"],
-    demo: "https://gondrak08.github.io/Asteroid-game/",
-    code: "https://github.com/Gondrak08/Asteroid-game",
   },
 ];
 
-export default storeCards;
+export default Works;
